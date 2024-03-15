b07214272d71de190d3f975bafef1d67e952948f
Sedanur Çevik <sdnrcvk@gmail.com>
2024-03-15 22:30:10 +0300

    feat: design header component

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -1,6 +1,166 @@
-export default function Header() {
-    return (
-        <header>Header component</header>
-    );
+"use client";
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { FaShoppingCart } from "react-icons/fa";
+import Link from 'next/link';
+
+interface DropdownContentProps {
+  open: boolean;
+}
+
+const HeaderContainer = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #FBA834;
+  height: 5rem;
+`;
+
+const Logo = styled.div`
+  display: flex;
+  align-items: center;
+  height: 5rem;
+  margin-left: 1rem;
+`;
+
+const LogoText = styled.span`
+  font-weight: bold;
+  font-size: 1.5rem;
+  cursor: pointer;
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  align-items: center;
+  height: 5rem;
+  margin-right: 1rem;
+`;
+
+const NavItem = styled.div`
+  padding: 0.5rem 1rem;
+  color: #333;
+  text-decoration: none;
+  font-size: 1.2rem;
+  border-bottom: none;
+  position: relative;
+`;
+
+const DropdownMenu = styled.div`
+  position: relative;
+  cursor: pointer;
+`;
+
+const DropdownContent = styled.div<DropdownContentProps>`
+  display: ${({ open }) => (open ? 'block' : 'none')};
+  position: absolute;
+  background-color: #fff;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  z-index: 1;
+  width: 150px;
+  margin-top:0.2rem;
+`;
+
+const DropdownItem = styled.a`
+  display: block;
+  padding: 0.5rem 1rem;
+  color: #333;
+  text-decoration: none;
+  font-size: 1rem;
+
+  &:hover {
+    background-color: #f0f0f0;
   }
-  
+`;
+
+const CartIcon = styled.div`
+  position: relative;
+  cursor: pointer;
+`;
+
+const CartCount = styled.span`
+  position: absolute;
+  top: -8px;
+  right: -8px;
+  background-color: #ff5722;
+  color: #fff;
+  font-size: 0.8rem;
+  padding: 0.1rem 0.4rem;
+  border-radius: 50%;
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: #000;
+
+  &:focus,
+  &:hover,
+  &:visited,
+  &:link,
+  &:active {
+    text-decoration: none;
+  }
+`;
+
+const NavItemLink = styled(Link)`
+  text-decoration: none;
+  color: #000;
+
+  &:focus,
+  &:hover,
+  &:visited,
+  &:link,
+  &:active {
+    text-decoration: none;
+  }
+
+  &:hover {
+    font-weight: bold;
+  }
+`;
+
+const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
+  return (
+    <HeaderContainer>
+      <Logo>
+        <StyledLink href="/">
+          <LogoText>sdnrcvk</LogoText>
+        </StyledLink>
+      </Logo>
+      <Nav>
+        <NavItem>
+          <NavItemLink href="/">Ana Sayfa</NavItemLink>
+        </NavItem>
+        <NavItem onMouseEnter={toggleMenu} onMouseLeave={closeMenu}>
+          <NavItemLink href="/#">Kategoriler</NavItemLink>
+          <DropdownMenu>
+            <DropdownContent open={isMenuOpen}>
+              <DropdownItem href="#kategori1">Kategori 1</DropdownItem>
+              <DropdownItem href="#kategori2">Kategori 2</DropdownItem>
+              <DropdownItem href="#kategori3">Kategori 3</DropdownItem>
+            </DropdownContent>
+          </DropdownMenu>
+        </NavItem>
+        <NavItem>
+          <StyledLink href="/cart">
+          <CartIcon>
+            <FaShoppingCart size={24} />
+            <CartCount>3</CartCount>
+          </CartIcon>
+          </StyledLink>
+        </NavItem>
+      </Nav>
+    </HeaderContainer>
+  );
+};
+
+export default Header;
