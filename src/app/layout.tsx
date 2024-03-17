829081302ae883182fdb21f420d6311bb5c6a7e0
Sedanur Çevik <sdnrcvk@gmail.com>
2024-03-17 22:18:57 +0300

    feat : use redux for cart

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
+import Providers from "./redux/Providers"
+import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,12 +19,18 @@ export default function RootLayout({
 }>) {
   return (
     <>
-    <html lang="en">
-      <body className={inter.className}>
-        <Header/>
-        {children}
+      <html lang="en">
+        <body className={inter.className}>
+          <Providers>
+          <Toaster
+              position="top-center"
+              reverseOrder={true}
+            />
+            <Header/>
+            {children}
+          </Providers>
         </body>
-    </html>
+      </html>
     </>
   );
 }
---
+++ src/app/redux/slices/cartSlice.js
@@ -0,0 +1,48 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+//Create Initial State
+const initialState = [];
+
+//Create the slice with Reducers
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart: (state, action) => {
+      const { id, title, brand, price, image } = action.payload;
+      // Check if the item already exists in the cart
+      const existingItem = state.find((item) => item.id === id);
+
+      if (existingItem) {
+        // If the item exists, update the quantity
+        existingItem.quantity += 1;
+      } else {
+        // If the item doesn't exist, add it to the cart
+        state.push({ id, title, brand, price, quantity: 1, image });
+      }
+    },
+    removeFromCart: (state, action) => {
+      const cartId = action.payload;
+      return state.filter((item) => item.id !== cartId);
+    },
+    incrementQty: (state, action) => {
+      const cartId = action.payload;
+      const cartItem = state.find((item) => item.id === cartId);
+      if (cartItem) {
+        cartItem.quantity += 1;
+      }
+    },
+    decrementQty: (state, action) => {
+      const cartId = action.payload;
+      const cartItem = state.find((item) => item.id === cartId);
+      if (cartItem && cartItem.quantity > 1) {
+        cartItem.quantity -= 1;
+      }
+    },
+  },
+});
+
+//export the reducers(actions)
+export const { addToCart, removeFromCart, incrementQty, decrementQty } =
+  cartSlice.actions;
+export default cartSlice.reducer;
