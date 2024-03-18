7919000ac0e49c193e397e0efe5580ecace0c23a
Sedanur Çevik <sdnrcvk@gmail.com>
2024-03-18 22:13:02 +0300

    feat : add components to home page

--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@ module.exports = {
   images: {
     domains: ['images.unsplash.com','www.export.org.uk'],
   },
+  compiler: {
+    // Enables the styled-components SWC transform
+    styledComponents: true
+  }
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,33 @@
-export default function Home() {
+"use client"
+import React from 'react';
+import CategoryProducts from './components/CategoryProducts';
+import SimpleSlider from './components/Slider';
+import styled from 'styled-components';
+
+const ContainerWrapper = styled.div`
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  background-color: #fff;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 1250px;
+  padding:1.2rem;
+  background-color: #F5F5F5;
+`;
+
+const Home = () => {
   return (
-    <h1>
-        Home PAGE
-    </h1>
+    <div className="home">
+      <SimpleSlider/>
+      <ContainerWrapper>
+        <CategoryProducts categoryId={1} />
+        <CategoryProducts categoryId={2} />
+      </ContainerWrapper>
+    </div>
   );
-}
+};
+
+export default Home;
